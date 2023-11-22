c14f8b9be91f73881211eb14ec5b725111502f5e
Nicolò Filippini <127590090+NicoFili06@users.noreply.github.com>
2023-11-22 08:31:39 +0100

    Add files via upload

---
+++ filippini.nicolò.4i.RubricaWPF/filippini.nicolò.4i.rubrica/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace filippini.nicolò._4i
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            int idx = 0;
+            Contatto[] Contatti = new Contatto[100];
+
+            try
+            {
+                StreamReader buffer = new StreamReader("Dati.csv");
+
+                for (int i = 0; i < Contatti.Length; i++)
+                    if (Contatti[i] == null)
+                        Contatti[i] = new Contatto();
+
+                idx = 0;
+
+                buffer.ReadLine();
+
+                while (!buffer.EndOfStream)
+                {
+                    string row = buffer.ReadLine();
+                    Contatto contatto = new Contatto(row);
+
+                    Contatti[idx++] = contatto;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+            dgArray.ItemsSource = Contatti;
+
+        }
+
+        private void dgArray_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+
+            Contatto? c = e.Row.Item as Contatto;
+            if (c != null)
+                if (c.Numero == 0)
+                {
+                    e.Row.Background = Brushes.Red;
+                    e.Row.Foreground = Brushes.White;
+                }
+
+
+        }
+    }
+}
---
+++ filippini.nicolò.4i.RubricaWPF/filippini.nicolò.4i.rubrica/contatto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filippini.nicolò._4i
+{
+    class Contatto
+    {
+        //Attributi
+        private string _nome;
+        private string _cognome;
+        private string _email;
+        private int _numero;
+        private string _cellulare;
+
+        //Costruttori
+
+        //Costruzione di un contatto tramite il file CSV
+        public Contatto(string row)
+        {
+            string[] fields = row.Split(';');
+
+            if (fields.Length >= 5)
+            {
+                _numero = 0;
+                int.TryParse(fields[0], out _numero);
+
+                _nome = fields[1];
+                _cognome = fields[2];
+                _cellulare = fields[3];
+                _email = fields[4];
+            }
+        }
+        //Costruzione di un contatto vuoto
+        public Contatto() { }
+
+
+        //Properties
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Cognome { get => _cognome; set => _cognome = value; }
+        public string Cellulare { get => _cellulare; set => _cellulare = value; }
+        public int Numero { get => _numero; set => _numero = value; }
+    }
+}
